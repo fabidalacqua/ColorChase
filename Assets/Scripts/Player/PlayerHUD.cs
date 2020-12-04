c0d4f71623f9dfd9d6f16379fce1752aa22c1efb
Fabiana Dalacqua <fabiana.dalacqua@gmail.com>
2020-12-03 22:10:28 -0300

    Player HUD and panels for panels for winner and between matchs

---
+++ Assets/Scripts/Player/PlayerHUD.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour
+{
+    [SerializeField]
+    private Image _characterBase;
+
+    [SerializeField]
+    private Image _characterFront;
+
+    [SerializeField]
+    private Image _deadPlayer;
+
+    [SerializeField]
+    private Sprite _noHeart;
+
+    [SerializeField]
+    private Image[] _hearts;
+
+    //TODO: Update life and player color during the game
+}
